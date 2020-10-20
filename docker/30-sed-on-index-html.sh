ea0d88a4c6f42b5fd046996962a8c52b31e6cf92
Emil Madsen <sovende@gmail.com>
2020-10-20 12:56:08 +0200

    Fix docker build

--- docker/30-sed-on-index-html.sh
+++ docker/30-sed-on-index-html.sh
@@ -2,4 +2,18 @@
 
 set -e
 
-sed "s#var GLOBAL_API_URL = '.*'#var GLOBAL_API_URL = '${GLOBAL_API_URL}'#g" -i /usr/share/nginx/html/index.html
+ME=$(basename $0)
+
+replace_global_url() {
+  local index_file="/usr/share/nginx/html/index.html"
+
+  if [ ! -f $index_file ]; then
+    echo >&3 "$ME: ERROR: $index_file does not exist"
+    return 0
+  fi
+  sed "s#var GLOBAL_API_URL = '.*'#var GLOBAL_API_URL = '${GLOBAL_API_URL}'#g" -i $index_file
+}
+
+replace_global_url
+
+exit 0
