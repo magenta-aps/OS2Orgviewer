76d5c73f4a2a449dcd920d32aa0559f15bf02ec7
Emil Madsen <sovende@gmail.com>
2020-10-20 13:52:32 +0200

    Default to 'default'

--- docker/25-symlink-html.sh
+++ docker/25-symlink-html.sh
@@ -7,9 +7,9 @@ ME=$(basename $0)
 symlink_html() {
   CUSTOMER_OPTIONS=$(ls /code/dist/ | tr '\n' ' ')
   if [ -z "$CUSTOMER" ]; then
-    echo >&3 "$ME: ERROR: \$CUSTOMER not set!"
-    echo >&3 "$ME: ERROR: \$CUSTOMER should be one of: $CUSTOMER_OPTIONS"
-    exit 1
+    echo >&3 "$ME: WARNING: \$CUSTOMER not set! Using default"
+    echo >&3 "$ME: WARNING: \$CUSTOMER should be one of: $CUSTOMER_OPTIONS"
+    CUSTOMER="default"
   fi
   CUSTOMER_FOLDER=/code/dist/$CUSTOMER
   if [ ! -d "$CUSTOMER_FOLDER" ]; then
