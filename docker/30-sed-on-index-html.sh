36f74bbb5111970664483f4a1f2087810d8bce64
Emil Madsen <sovende@gmail.com>
2020-10-22 16:16:41 +0200

    Disable whitelist, template GLOBAL_API_ROOT_UUID

--- docker/30-sed-on-index-html.sh
+++ docker/30-sed-on-index-html.sh
@@ -14,6 +14,17 @@ replace_global_url() {
   sed "s#var GLOBAL_API_URL = '.*'#var GLOBAL_API_URL = '${GLOBAL_API_URL}'#g" -i $index_file
 }
 
+replace_global_root_uuid() {
+  local index_file="/usr/share/nginx/html/index.html"
+
+  if [ ! -f $index_file ]; then
+    echo >&3 "$ME: ERROR: $index_file does not exist"
+    exit 1
+  fi
+  sed "s#var GLOBAL_API_ROOT_UUID = '.*'#var GLOBAL_API_ROOT_UUID = '${GLOBAL_API_ROOT_UUID}'#g" -i $index_file
+}
+
 replace_global_url
+replace_global_root_uuid
 
 exit 0
