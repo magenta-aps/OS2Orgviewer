7d296bb03d2d2bcc660980f7b9c2ff7c3d82bafe
iamfrank <frank@magenta.dk>
2020-10-28 17:54:02 +0100

    hide "secret" address info from user

--- app/src/components/address/AddressList.vue
+++ app/src/components/address/AddressList.vue
@@ -2,27 +2,29 @@
 
     <dl class="oc-address-list">
         <template v-for="address in list">
-            <dt :key="address.address_type.uuid">{{ address.address_type.name }}</dt>
-            <dd :key="address.uuid">
-                <a 
-                    v-if="address.address_type.name === 'Email'"
-                    :href="`mailto:${ address.name }`">
-                    {{ address.name }}
-                </a>
-                <a 
-                    v-else-if="address.address_type.name === 'Webadresse' || address.address_type.name === 'Dokumentadresse'"
-                    :href="address.name">
-                    {{ address.name }}
-                </a>
-                <a 
-                    v-else-if="address.address_type.name === 'Telefon'"
-                    :href="`tel:${ address.name}`">
-                    {{ address.name }}
-                </a>                
-                <span v-else>
-                    {{ address.name }}
-                </span>
-            </dd>
+            <template v-if="!address.visibility || address.visibility.name !== 'Hemmelig'">
+                <dt :key="address.address_type.uuid">{{ address.address_type.name }}</dt>
+                <dd :key="address.uuid">
+                    <a 
+                        v-if="address.address_type.name === 'Email'"
+                        :href="`mailto:${ address.name }`">
+                        {{ address.name }}
+                    </a>
+                    <a 
+                        v-else-if="address.address_type.name === 'Webadresse' || address.address_type.name === 'Dokumentadresse'"
+                        :href="address.name">
+                        {{ address.name }}
+                    </a>
+                    <a 
+                        v-else-if="address.address_type.name === 'Telefon'"
+                        :href="`tel:${ address.name}`">
+                        {{ address.name }}
+                    </a>                
+                    <span v-else>
+                        {{ address.name }}
+                    </span>
+                </dd>
+            </template>
         </template>
     </dl>
     
