776a159a117b4314b50b45a390004a6cddcc74df
iamfrank <frank@magenta.dk>
2021-05-07 10:38:00 +0200

    symlink should always be to /dist/

--- docker/25-symlink-html.sh
+++ docker/25-symlink-html.sh
@@ -5,20 +5,7 @@ set -e
 ME=$(basename $0)
 
 symlink_html() {
-  CUSTOMER_OPTIONS=$(ls /code/dist/ | tr '\n' ' ')
-  if [ -z "$CUSTOMER" ]; then
-    echo >&3 "$ME: WARNING: \$CUSTOMER not set! Using default"
-    echo >&3 "$ME: WARNING: \$CUSTOMER should be one of: $CUSTOMER_OPTIONS"
-    CUSTOMER="default"
-  fi
-  CUSTOMER_FOLDER=/code/dist
-  if [ ! -d "$CUSTOMER_FOLDER" ]; then
-    echo >&3 "$ME: ERROR: $CUSTOMER_FOLDER does not exist!"
-    echo >&3 "$ME: ERROR: \$CUSTOMER should be one of: $CUSTOMER_OPTIONS"
-    exit 1
-  fi
-
-  ln -s -f $CUSTOMER_FOLDER/* /usr/share/nginx/html
+  ln -s -f /code/dist/* /usr/share/nginx/html
 }
 
 symlink_html
