a2f3b86c6fe77f67fc31ae1f80235c8bdf1dbdd2
Andreas Kring <andreas@magenta-aps.dk>
2022-06-15 14:37:12 +0200

    Crash less often due to Kubernetes CrashLoopBackoff issues

--- docker/50-crash.sh
+++ docker/50-crash.sh
@@ -5,6 +5,10 @@
 # We have not learned how to manage keycloak tokens yet, and since orgviewer
 # has this half-baked-RBAC-in-nginx-implementation, we only get a fresh token
 # on startup. Well, the tokens expire so we just do the obvious thing; crash
-# every 10 minutes and let k8s/docker restart the process!
+# approximately every 10 minutes and let k8s/docker restart the process!
+# NOTE: do not set the CRASH_RESTART_BASE_TIME too low as this will lead to
+# CrashLoopBackoff issues in Kubernetes
+# (see https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/#restart-policy)
+# since the backoff delay will increase if the container is running less than 10 minutes
 
-(sleep $((200 + $RANDOM % 100)) && kill 1)&
+(sleep $(($CRASH_RESTART_BASE_TIME + $RANDOM % 100)) && kill 1)&
