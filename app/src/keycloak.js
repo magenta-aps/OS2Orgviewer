7e3cab308157ca00eaddd07c4bdd282ba54a19d1
Carl Bordum Hansen <carl@bordum.dk>
2022-09-15 15:07:33 +0200

    [#51382] Remove refresh recursion loop

--- app/src/keycloak.js
+++ app/src/keycloak.js
@@ -4,27 +4,31 @@ const clientId = 'orgviewer'
 const realm = 'mo'
 
 export async function initKeycloak() {
-  const url = `${store.state.global_api_url}/auth`
-  const clientSecret = store.state.keycloak_client_secret
+  async function refreshToken() {
+    const url = `${store.state.global_api_url}/auth`
+    const clientSecret = store.state.keycloak_client_secret
 
-  const keycloak_url = `${url}/realms/${realm}/protocol/openid-connect/token`
+    const keycloak_url = `${url}/realms/${realm}/protocol/openid-connect/token`
 
-  const res = await fetch(
-      keycloak_url, {
-      method: 'POST',
-      body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+    const res = await fetch(
+        keycloak_url, {
+        method: 'POST',
+        body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
       }
-    }
-  )
+    )
 
-  const json = await res.json()
-  store.commit('setAccessToken', json.access_token)
+    const json = await res.json()
+    store.commit('setAccessToken', json.access_token)
+  }
 
-  // Token refresh
+  await refreshToken()
+
+  // Token refresh continously
   setInterval(() => {
-    initKeycloak().catch(() => {
+    refreshToken().catch(() => {
       console.error("Failed to refresh token")
     })
   }, 60000)
