7af44a036b83382ef6ac064295dfbcc5edcdc887
Mathias Dannesbo (Magenta) <md@magenta.dk>
2022-10-12 14:52:15 +0200

    [#51382] Serve assets under a relative path
    This enables the application to be served under a sub-path by the reverse-proxy.
    
    https://cli.vuejs.org/config/#publicpath

--- app/vue.config.js
+++ app/vue.config.js
@@ -4,6 +4,7 @@ process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
     outputDir: 'dist',
+    publicPath: '', // serve assets under a relative path.
     productionSourceMap: false,
     transpileDependencies: [
         'non-layered-tidy-tree-layout'
